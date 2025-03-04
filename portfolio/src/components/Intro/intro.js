732644208fcbbd37b4345fc7fe356b64ae0978bb
MUHSINA <muhsinashinwari@gmail.com>
2025-03-04 14:41:07 -0500

    starter files added

---
+++ portfolio/src/components/Intro/intro.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './intro.css';
+
+
+const Intro = () => {
+    return(
+        <section id ="intro">
+            <div className="introContent">
+                <span className="hello"> Hello </span>
+                <span className="IntroText">I'm <span className="introName">Muhsina</span><br/> Website Designer</span>    
+                <p className="introPara">I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores.</p>
+    
+                </div>
+                <img src ="" alt="Portfoilio" className="bg"/>
+        </section>
+      
+    )
+}   
+export default Intro;
+
+
